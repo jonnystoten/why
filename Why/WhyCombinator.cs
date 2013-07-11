ae34f2d40b0987a090547455b0e61a309acf2fa7
Jonny Stoten <applechewer@gmail.com>
2013-07-11 22:40:39 +0100

    Allow different types of args and result

--- Why/WhyCombinator.cs
+++ Why/WhyCombinator.cs
@@ -4,29 +4,29 @@ namespace Why
 {
     public class WhyCombinator
     {
-        private static Func<T, T> Y<T>(Func<Func<T, T>, Func<T, T>> le)
+        private static Func<TArg, TResult> Y<TArg, TResult>(Func<Func<TArg, TResult>, Func<TArg, TResult>> le)
         {
-            return A<T>(f => B(le, f));
+            return A<TArg, TResult>(f => B(le, f));
         }
 
-        private static Func<T, T> A<T>(Func<dynamic, Func<T, T>> f)
+        private static Func<TArg, TResult> A<TArg, TResult>(Func<dynamic, Func<TArg, TResult>> f)
         {
             return f(f);
         }
 
-        private static Func<T, T> B<T>(Func<Func<T, T>, Func<T, T>> le, Func<dynamic, Func<T, T>> f)
+        private static Func<TArg, TResult> B<TArg, TResult>(Func<Func<TArg, TResult>, Func<TArg, TResult>> le, Func<dynamic, Func<TArg, TResult>> f)
         {
             return le(x => C(f, x));
         }
 
-        private static T C<T>(Func<dynamic, Func<T, T>> f, T x)
+        private static TResult C<TArg, TResult>(Func<dynamic, Func<TArg, TResult>> f, TArg x)
         {
             return A(f)(x);
         }
 
         public int FactorialUsingY(int x)
         {
-            var factorial = Y<int>(fac => n => n <= 2 ? n : n * fac(n - 1));
+            var factorial = Y<int, int>(fac => n => n <= 2 ? n : n * fac(n - 1));
             return factorial(x);
         }
     }
