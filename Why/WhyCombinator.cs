1c912de93563f4835c38a1577f587c0ce51204af
Jonny Stoten <applechewer@gmail.com>
2013-07-11 21:59:51 +0100

    First attempt implementing 'Y'
    from http://javascript.crockford.com/little.html

---
+++ Why/WhyCombinator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Why
+{
+    public class WhyCombinator
+    {
+        private static dynamic Y(Func<Func<dynamic, dynamic>, Func<dynamic, dynamic>> le)
+        {
+            Func<Func<dynamic, dynamic>, dynamic> func = f => f(f);
+            return func(f => le(x => f(f)(x)));
+        }
+
+        public int FactorialUsingY(int x)
+        {
+            var factorial = Y(fac => n => n <= 2 ? n : n * fac(n - 1));
+            return factorial(x);
+        }
+    }
+}
---
+++ WhyTests/WhyCombinatorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Why;
+
+namespace WhyTests
+{
+    [TestFixture]
+    public class WhyCombinatorTests
+    {
+        [Test]
+        public void Test()
+        {
+            var why = new WhyCombinator();
+
+            Assert.That(why.FactorialUsingY(5), Is.EqualTo(120));
+        }
+    }
+}
